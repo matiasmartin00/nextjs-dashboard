24c8edd022e8e654d62b0e0cfc0d9cc7c16cfa5e
matiasmartin <matiass.lean@gmail.com>
2024-12-29 17:39:00 +0100

    chore: Versel deployment

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -29,7 +29,9 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
 
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={() => {
+      deleteInvoiceWithId();
+    }}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
