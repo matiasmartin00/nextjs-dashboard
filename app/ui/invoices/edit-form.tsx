1e358f6e07ff008ee593e33f50742d362984fd4a
matiasmartin <matiass.lean@gmail.com>
2024-12-29 17:41:31 +0100

    chore: Versel deployment

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -22,7 +22,9 @@ export default function EditInvoiceForm({
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={(form: FormData) => {
+      updateInvoiceWithId(form);
+    }}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
